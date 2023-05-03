a7e06512bf5b9def51fe84add02135ae93e2a2f0
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-03 19:52:41 +0330

    class components - state - change state - handling events - setState method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from "react";
 import Products from "./components/Products/Products";
 
-const App = () => {
-  return (
-    <div id="main" className="container">
-      <h2>React App</h2>
-      <h3>Products:</h3>
-      <Products name="Cake" price="65" />
-      <Products name="Coffee" price="78" />
-      <Products name="Tea" price="47">
-        Availibility: Available
-      </Products>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    Products: [
+      { name: "Tea & Cake", price: "79" },
+      { name: "Coffee & Cake", price: "99" },
+      { name: "Milk & Cake", price: "88" },
+    ],
+  };
+
+  changePriceHandler = () => {
+    this.setState({
+      Products: [
+        { name: "Tea & Cake", price: "71.1$  After 10% OFF" },
+        { name: "Coffee & Cake", price: "69.3$  After 30% OFF" },
+        { name: "Milk & Cake", price: "70.4$  After 20% OFF" },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>React App</h2>
+        <h3>Products:</h3>
+        <Products
+          name={this.state.Products[0].name}
+          price={this.state.Products[0].price}
+        />
+        <Products
+          name={this.state.Products[1].name}
+          price={this.state.Products[1].price}
+        />
+        <Products
+          name={this.state.Products[2].name}
+          price={this.state.Products[2].price}
+        />
+        <button onClick={this.changePriceHandler}>Change Price</button>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,9 +3,8 @@ import React from "react";
 const Products = (props) => {
   return (
     <div className="product-info">
-      <p className="product-name">Product Name:{props.name}</p>
-      <p className="product-price">Product Price:{props.price}</p>
-      <p>{props.children}</p>
+      <p className="product-name">Product Name: {props.name}</p>
+      <p className="product-price">Product Price: {props.price}</p>
       <hr></hr>
     </div>
   );
