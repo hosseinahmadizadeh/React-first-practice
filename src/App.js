6cf62cd541dede2882f729579a9dc193b8ade5d6
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-04 11:50:12 +0330

    use useState - act of change state with useState in function component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Products from "./components/Products/Products";
+import { useState } from "react";
 
-class App extends React.Component {
-  state = {
+const App = (props) => {
+  const [productState, setProductState] = useState({
     Products: [
       { name: "Tea & Cake", price: "79" },
       { name: "Coffee & Cake", price: "99" },
       { name: "Milk & Cake", price: "88" },
     ],
-  };
+  });
 
-  changePriceHandler = () => {
-    this.setState({
+  const changePriceHandler = () => {
+    setProductState({
       Products: [
         { name: "Tea & Cake", price: "71.1$  After 10% OFF" },
         { name: "Coffee & Cake", price: "69.3$  After 30% OFF" },
@@ -20,27 +21,67 @@ class App extends React.Component {
     });
   };
 
-  render() {
-    return (
-      <div>
-        <h2>React App</h2>
-        <h3>Products:</h3>
-        <Products
-          name={this.state.Products[0].name}
-          price={this.state.Products[0].price}
-        />
-        <Products
-          name={this.state.Products[1].name}
-          price={this.state.Products[1].price}
-        />
-        <Products
-          name={this.state.Products[2].name}
-          price={this.state.Products[2].price}
-        />
-        <button onClick={this.changePriceHandler}>Change Price</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div id="main" className="container">
+      <h2>React App</h2>
+      <h3>Products:</h3>
+      <Products
+        name={productState.Products[0].name}
+        price={productState.Products[0].price}
+      />
+      <Products
+        name={productState.Products[1].name}
+        price={productState.Products[1].price}
+      />
+      <Products
+        name={productState.Products[2].name}
+        price={productState.Products[2].price}
+      />
+      <button onClick={changePriceHandler}>Change Price</button>
+    </div>
+  );
+};
+
+// class App extends React.Component {
+//   state = {
+//     Products: [
+//       { name: "Tea & Cake", price: "79" },
+//       { name: "Coffee & Cake", price: "99" },
+//       { name: "Milk & Cake", price: "88" },
+//     ],
+//   };
+
+//   changePriceHandler = () => {
+//     this.setState({
+//       Products: [
+//         { name: "Tea & Cake", price: "71.1$  After 10% OFF" },
+//         { name: "Coffee & Cake", price: "69.3$  After 30% OFF" },
+//         { name: "Milk & Cake", price: "70.4$  After 20% OFF" },
+//       ],
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div id="main" className="container">
+//         <h2>React App</h2>
+//         <h3>Products:</h3>
+//         <Products
+//           name={this.state.Products[0].name}
+//           price={this.state.Products[0].price}
+//         />
+//         <Products
+//           name={this.state.Products[1].name}
+//           price={this.state.Products[1].price}
+//         />
+//         <Products
+//           name={this.state.Products[2].name}
+//           price={this.state.Products[2].price}
+//         />
+//         <button onClick={this.changePriceHandler}>Change Price</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
