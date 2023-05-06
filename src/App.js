87484d7feed8125416b43dce2cca50b6e102dce2
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-06 18:54:53 +0330

    updatig state better approach

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   };
 
   deleteProductHandler = (productIndex) => {
-    const products = this.state.products;
+    const products = [...this.state.products];
     products.splice(productIndex, 1);
     this.setState({ products: products });
   };
