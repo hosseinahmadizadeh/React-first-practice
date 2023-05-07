e0b06a850cf7944904c37ddb9ebeb652dcdd04ea
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-07 18:31:45 +0330

    splitting code into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Products from "./components/Products/Products";
-// import { useState } from "react";
+import ProductsList from "./components/ProductsList/ProductsList";
+import Main from "./components/Main/Main";
 import "./App.css";
 
 class App extends React.Component {
@@ -42,43 +42,22 @@ class App extends React.Component {
   };
 
   render() {
-    const btn = {
-      backgroundColor: "#7b1fa2",
-      color: "#ffffff",
-      font: "inherit",
-      border: "none",
-      outline: "none",
-      borderRadius: "3px",
-      padding: "0.6rem",
-      margin: "0.6rem auto",
-    };
-
     let products = null;
     if (this.state.showProducts) {
       products = (
         <div>
-          {this.state.products.map((item, index) => {
-            return (
-              <Products
-                click={() => this.deleteProductHandler(index)}
-                name={item.name}
-                price={item.price}
-                key={item.id}
-                change={(event) => this.changeNameHandler(event, item.id)}
-              />
-            );
-          })}
+          <ProductsList
+            products={this.state.products}
+            click={this.deleteProductHandler}
+            change={this.changeNameHandler}
+          />
         </div>
       );
     }
 
     return (
       <div id="main" className="container">
-        <h2>Cafe</h2>
-        <h3>Products:</h3>
-        <button style={btn} onClick={this.toggleProductsHandler}>
-          Show/Hide Products
-        </button>
+        <Main click={this.toggleProductsHandler} />
         {products}
       </div>
     );
@@ -86,43 +65,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// const App = (props) => {
-//   const [productState, setProductState] = useState({
-//     Products: [
-//       { name: "Tea & Cake", price: "79" },
-//       { name: "Coffee & Cake", price: "99" },
-//       { name: "Milk & Cake", price: "88" },
-//     ],
-//   });
-
-//   const changePriceHandler = () => {
-//     setProductState({
-//       Products: [
-//         { name: "Tea & Cake", price: "71.1$  After 10% OFF" },
-//         { name: "Coffee & Cake", price: "69.3$  After 30% OFF" },
-//         { name: "Milk & Cake", price: "70.4$  After 20% OFF" },
-//       ],
-//     });
-//   };
-
-//   return (
-//     <div id="main" className="container">
-//       <h2>React App</h2>
-//       <h3>Products:</h3>
-//       <Products
-//         name={productState.Products[0].name}
-//         price={productState.Products[0].price}
-//       />
-//       <Products
-//         name={productState.Products[1].name}
-//         price={productState.Products[1].price}
-//       />
-//       <Products
-//         name={productState.Products[2].name}
-//         price={productState.Products[2].price}
-//       />
-//       <button onClick={changePriceHandler}>Change Price</button>
-//     </div>
-//   );
-// };
---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Products from "../Products/Products";
+
+const ProductsList = (props) => {
+  return props.products.map((item, index) => {
+    return (
+      <Products
+        click={() => props.delete(index)}
+        name={item.name}
+        price={item.price}
+        key={item.id}
+        change={(event) => props.change(event, item.id)}
+      />
+    );
+  });
+};
+
+export default ProductsList;
