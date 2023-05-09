b08dcd97fda6885983dc3acb7ede37ff9b9857f9
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-09 17:52:25 +0330

    rendering adjacent jsx elements 1

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,19 +4,20 @@ import "./Products.css";
 class Products extends Component {
   render() {
     console.log("Products");
-    return (
-      <div className="product-info">
-        <p className="product-name" onClick={this.props.click}>
-          Product Name: {this.props.name}
-        </p>
-        <p className="product-price">Product Price: {this.props.price}</p>
-        <input
-          type="text"
-          onChange={this.props.change}
-          value={this.props.name}
-        />
-      </div>
-    );
+    return [
+      <p key="1" className="product-name" onClick={this.props.click}>
+        , Product Name: {this.props.name}
+      </p>,
+      <p key="2" className="product-price">
+        Product Price: {this.props.price}
+      </p>,
+      <input
+        key="3"
+        type="text"
+        onChange={this.props.change}
+        value={this.props.name}
+      />,
+    ];
   }
 }
 
